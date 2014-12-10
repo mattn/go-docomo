d946966b13b596ca22137e0dcf3ebfd0fcfd41ec
mattn <mattn.jp@gmail.com>
2014-12-10 18:50:47 +0900

    page

--- docomo-lingrbot/main.go
+++ docomo-lingrbot/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/mattn/go-docomo"
 	"github.com/mattn/go-lingr"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -31,34 +32,44 @@ func main() {
 
 	nick := cfg.User.Nickname
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/lingr" {
-			if r.Method != "POST" {
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-			status, err := lingr.DecodeStatus(r.Body)
+		if r.URL.Path == "/" {
+			f, err := os.Open("index.html")
 			if err != nil {
-				http.Error(w, err.Error(), 500)
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				return
 			}
-			for _, event := range status.Events {
-				m := event.Message
-				if m == nil {
-					continue
-				}
-				if !strings.HasPrefix(m.Text, nick+":") {
-					continue
-				}
-				text := m.Text[len(nick)+2:]
-				res, err := c.Conversation(text)
-				if err != nil {
-					log.Fatal(err)
-				}
-				w.Header().Set("Content-Type", "text/plain")
-				w.Write([]byte(fmt.Sprintf("%s: %s", m.Nickname, res.Utt)))
+			w.Header().Set("Content-Type", "text/html")
+			io.Copy(w, f)
+			return
+		}
+		if r.URL.Path != "/lingr" {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		if r.Method != "POST" {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		status, err := lingr.DecodeStatus(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		for _, event := range status.Events {
+			m := event.Message
+			if m == nil {
+				continue
 			}
-		} else {
-			http.NotFound(w, r)
+			if !strings.HasPrefix(m.Text, nick+":") {
+				continue
+			}
+			text := m.Text[len(nick)+2:]
+			res, err := c.Conversation(text)
+			if err != nil {
+				log.Fatal(err)
+			}
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte(fmt.Sprintf("%s: %s", m.Nickname, res.Utt)))
 		}
 	})
 	http.ListenAndServe(cfg.Addr, nil)
